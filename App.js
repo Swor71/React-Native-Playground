13eae13f7b9b47f68bc5f21574c041dd5b3c8fba
Marcin Swornowski <swor71@gmail.com>
2020-06-09 19:12:29 +0200

    Add button for testing purposes

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('first text.')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <Button title="Press me" onPress={() => setText('second text')} />
     </View>
   );
 }
