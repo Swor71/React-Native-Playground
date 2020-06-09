8c9a601a255b248f66f6fdc8a38c5ffe413269f0
Marcin Swornowski <swor71@gmail.com>
2020-06-09 19:18:04 +0200

    Add random text selection

--- App.js
+++ App.js
@@ -4,10 +4,22 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   const [text, setText] = useState('first text.')
 
+  const optionsArray = [
+    'first text',
+    'second text',
+    'third text',
+  ];
+
+  const changeContents = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    setText(optionsArray[randomNumber])
+  }
+
   return (
     <View style={styles.container}>
       <Text>{text}</Text>
-      <Button title="Press me" onPress={() => setText('second text')} />
+      <Button title="Press me" onPress={() => changeContents()} />
     </View>
   );
 }
