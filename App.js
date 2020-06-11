345d9ae62c330fa3e15a8e23aee93593331cdcf7
Marcin Swornowski <swor71@gmail.com>
2020-06-11 18:40:56 +0200

    Add styling to a todo

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [todo, setTodo] = useState('buy milk');
-  const [todoList, setTodoList] = useState([])
+  const [todo, setTodo] = useState("buy milk");
+  const [todoList, setTodoList] = useState([]);
 
-  const handleTextChange = value => setTodo(value);
+  const handleTextChange = (value) => setTodo(value);
 
-  const handleButtonPress = () => setTodoList(prevState => [...prevState, todo]);
+  const handleButtonPress = () =>
+    setTodoList((prevState) => [...prevState, todo]);
 
   return (
     <View style={styles.root}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Add a todo item" style={styles.input} onChangeText={handleTextChange} value={todo} />
+        <TextInput
+          placeholder="Add a todo item"
+          style={styles.input}
+          onChangeText={handleTextChange}
+          value={todo}
+        />
         <Button title="press" onPress={handleButtonPress} />
       </View>
       <View>
-        {todoList.map(todo => <Text key={todo}>{todo}</Text>)}
+        {todoList.map((todo) => (
+          <View style={styles.todoItem} key={todo}>
+            <Text>{todo}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -27,11 +37,21 @@ const styles = StyleSheet.create({
     padding: 50,
   },
   input: {
-    borderBottomColor: 'black', borderBottomWidth: 1, padding: 5, width: '70%'
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    padding: 5,
+    width: "70%",
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  }
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  todoItem: {
+    marginVertical: 5,
+    padding: 5,
+    backgroundColor: "#eee",
+    borderColor: "#aaa",
+    borderWidth: 1,
+  },
 });
