760c0fe552c66c9ce0a0bfdf5fd8e5527208c343
Marcin Swornowski <swor71@gmail.com>
2020-06-12 18:42:02 +0200

    Add modal to add a todo

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Button } from "react-native";
 import { TodoInput } from "./components/TodoInput";
 import { TodoListItem } from "./components/TodoListItem";
 
 export default function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isModalActive, setIsModalActive] = useState(false);
 
   const handleButtonPress = todo => () => {
     setTodoList(prevStateTodoList => [
       ...prevStateTodoList,
       { key: Math.random().toString(), value: todo },
     ]);
+    setIsModalActive(false);
   };
 
   const handleDeleteItem = itemId => () => {
@@ -19,9 +21,18 @@ export default function App() {
     );
   };
 
+  const handleShowModal = () => {
+    setIsModalActive(prevStateIsModalActive => !prevStateIsModalActive);
+  };
+
   return (
     <View style={styles.root}>
-      <TodoInput handleButtonPress={handleButtonPress} />
+      <Button title="add a todo" onPress={handleShowModal} />
+      <TodoInput
+        isModalActive={isModalActive}
+        handleButtonPress={handleButtonPress}
+        handleShowModal={handleShowModal}
+      />
       <FlatList
         data={todoList}
         renderItem={({ item }) => (
--- components/TodoInput.js
+++ components/TodoInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, TextInput, StyleSheet, Button } from "react-native";
+import { View, TextInput, StyleSheet, Button, Modal } from "react-native";
 
 export const TodoInput = props => {
   const [todo, setTodo] = useState();
@@ -7,15 +7,23 @@ export const TodoInput = props => {
   const handleTextChange = value => setTodo(value);
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Add a todo item"
-        style={styles.input}
-        onChangeText={handleTextChange}
-        value={todo}
-      />
-      <Button title="press" onPress={props.handleButtonPress(todo)} />
-    </View>
+    <Modal visible={props.isModalActive} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Add a todo item"
+          style={styles.input}
+          onChangeText={handleTextChange}
+        />
+        <View style={styles.buttonContainer}>
+          <Button title="add a todo" onPress={props.handleButtonPress(todo)} />
+          <Button
+            title="cancel"
+            color="maroon"
+            onPress={props.handleShowModal}
+          />
+        </View>
+      </View>
+    </Modal>
   );
 };
 
@@ -25,10 +33,16 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     padding: 5,
     width: "70%",
+    marginBottom: 20,
   },
   inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
+    width: "50%",
   },
 });
